82ee63ab504bc4ee2c21b0eaf59ad52fe176a4d5
Shurkah <94490427+Shurkah@users.noreply.github.com>
2022-11-08 22:43:01 +0100

    Add files via upload
    This program creates a list of all possible outcomes when tossing a coin for n times.

---
+++ cointoss_combos.py
@@ -0,0 +1,34 @@
+"""This program creates a list of all possible outcomes/
+when tossing a coin for n times."""
+
+# there are two possible outcomes when tossing a coin: heads and tails
+coin = ['H', 'T']
+
+"""The following two lines are for user interface"""
+# print('Enter the number of times the coin is tossed: ')
+# times = int(input())
+
+"""For me it is faster to change one value here:"""
+times = 3
+
+
+def augment(outcomes):
+    """This function takes a list of possible outcomes of n tosses/
+    and creates a list of possible outcomes for n+1 tosses, /
+    thus augmenting the toss count by 1"""
+    new_list = []
+    for outcome in outcomes:
+        for side in coin:
+            new_list.append(outcome + side)
+    return new_list
+
+
+def tosses(times, list):
+    """This function repeats augmenting for the user-entered number of times"""
+    for i in range(times-1):
+        list = augment(list)
+    return list
+
+
+print(*tosses(times, coin), sep='\n')
+
